f69c4c8e2a82571f1aa27c88755038c9a0291e5b
Christian Criado <cmc1476@gmail.com>
2025-12-05 16:45:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const canvas = document.getElementById("superficie");
 const ctx = canvas.getContext("2d");
 
+// Barrido horizontal
 let y = 0;
-const speed = 1.5; // velocidad del barrido
-const barHeight = 25; // altura de la barra blanca
+const speedY = 1.5;
+const barHeight = 25;
+
+// Barrido vertical
+let x = 0;
+const speedX = 1.5;
+const barWidth = 25;
 
 function animar() {
+    // Fondo negro
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // Barra horizontal
     ctx.fillStyle = "white";
     ctx.fillRect(0, y, canvas.width, barHeight);
 
-    y += speed;
+    // Barra vertical
+    ctx.fillRect(x, 0, barWidth, canvas.height);
+
+    // Actualizar posición horizontal
+    y += speedY;
     if (y > canvas.height) y = -barHeight;
+
+    // Actualizar posición vertical
+    x += speedX;
+    if (x > canvas.width) x = -barWidth;
 
     requestAnimationFrame(animar);
 }
 
 animar();
+
+
 
 
 
